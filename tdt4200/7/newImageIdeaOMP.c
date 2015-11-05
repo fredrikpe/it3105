94dab66e2da7f2d8e310b901484ad27cabaec49f
Fredrik Ingebrigtsen <fredrikpei@gmail.com>
2015-11-05 10:29:13 +0100

    omp ser ut til å fungere

--- tdt4200/7/newImageIdeaOMP.c
+++ tdt4200/7/newImageIdeaOMP.c
@@ -10,46 +10,136 @@
 // http://7-themes.com/6971875-funny-flowers-pictures.html
 
 typedef struct {
-    float red,green,blue;
+  float red,green,blue;
 } AccuratePixel;
 
 typedef struct {
-     int x, y;
-     AccuratePixel *data;
+  int x, y;
+  AccuratePixel *data;
 } AccurateImage;
 
 // Convert ppm to high precision format.
 AccurateImage *convertImageToNewFormat(PPMImage *image) {
-	// Make a copy
-	AccurateImage *imageAccurate;
-	imageAccurate = (AccurateImage *)malloc(sizeof(AccurateImage));
-	imageAccurate->data = (AccuratePixel*)malloc(image->x * image->y * sizeof(AccuratePixel));
-	for(int i = 0; i < image->x * image->y; i++) {
-		imageAccurate->data[i].red   = (float) image->data[i].red;
-		imageAccurate->data[i].green = (float) image->data[i].green;
-		imageAccurate->data[i].blue  = (float) image->data[i].blue;
-	}
-	imageAccurate->x = image->x;
-	imageAccurate->y = image->y;
+  // Make a copy
+  AccurateImage *imageAccurate;
+  imageAccurate = (AccurateImage *)malloc(sizeof(AccurateImage));
+  imageAccurate->data = (AccuratePixel*)malloc(image->x * image->y * sizeof(AccuratePixel));
+  for(int i = 0; i < image->x * image->y; i++) {
+    imageAccurate->data[i].red   = (float) image->data[i].red;
+    imageAccurate->data[i].green = (float) image->data[i].green;
+    imageAccurate->data[i].blue  = (float) image->data[i].blue;
+  }
+  imageAccurate->x = image->x;
+  imageAccurate->y = image->y;
 
-	return imageAccurate;
+  return imageAccurate;
 }
 
 AccurateImage *createEmptyImage(PPMImage *image){
-	AccurateImage *imageAccurate;
-	imageAccurate = (AccurateImage *)malloc(sizeof(AccurateImage));
-	imageAccurate->data = (AccuratePixel*)malloc(image->x * image->y * sizeof(AccuratePixel));
-	imageAccurate->x = image->x;
-	imageAccurate->y = image->y;
+  AccurateImage *imageAccurate;
+  imageAccurate = (AccurateImage *)malloc(sizeof(AccurateImage));
+  imageAccurate->data = (AccuratePixel*)malloc(image->x * image->y * sizeof(AccuratePixel));
+  imageAccurate->x = image->x;
+  imageAccurate->y = image->y;
 
-	return imageAccurate;
+  return imageAccurate;
 }
 
 // free memory of an AccurateImage
 void freeImage(AccurateImage *image){
-	free(image->data);
-	free(image);
+  free(image->data);
+  free(image);
 }
+
+void bar(int senterY, int size, AccuratePixel *line_buffer, AccurateImage* imageIn,
+          AccurateImage *imageOut)
+{
+
+  float sum_red = 0;
+  float sum_blue = 0;
+  float sum_green =0;
+  int W = imageIn->x;
+  int H = imageIn->y;
+
+  int starty = senterY-size;
+  int endy = senterY+size;
+
+
+  // Initialize and update the line_buffer.
+  // For OpenMP this might cause problems
+  // Separating out the initialization part might help
+  if (starty <=0) {
+    starty = 0;
+    for(int i=0; i<W; i++){
+      // add the next pixel of the next line in the column x
+      line_buffer[i].blue+=imageIn->data[W*endy+i].blue;
+      line_buffer[i].red+=imageIn->data[W*endy+i].red;
+      line_buffer[i].green+=imageIn->data[W*endy+i].green;
+    }
+  }
+
+  else if (endy >= H ){
+    // for the last lines, we just need to subtract the first added line
+    endy = H-1;
+    for(int i=0; i<W; i++){
+      line_buffer[i].blue-=imageIn->data[W*(starty-1)+i].blue;
+      line_buffer[i].red-=imageIn->data[W*(starty-1)+i].red;
+      line_buffer[i].green-=imageIn->data[W*(starty-1)+i].green;
+    }
+  }else{
+    // general case
+    // add the next line and remove the first added line
+    for(int i=0; i<W; i++){
+      line_buffer[i].blue+=imageIn->data[W*endy+i].blue-imageIn->data[W*(starty-1)+i].blue;
+      line_buffer[i].red+=imageIn->data[W*endy+i].red-imageIn->data[W*(starty-1)+i].red;
+      line_buffer[i].green+=imageIn->data[W*endy+i].green-imageIn->data[W*(starty-1)+i].green;
+    }
+  }
+  // End of line_buffer initialisation.
+
+
+  sum_green =0;
+  sum_red = 0;
+  sum_blue = 0;
+  for(int senterX = 0; senterX < W; senterX++) {
+    // in this loop, we do exactly the same thing as before but only with the array line_buffer
+
+    int startx = senterX-size;
+    int endx = senterX+size;
+
+    if (startx <=0){
+      startx = 0;
+      if(senterX==0){
+        for (int x=startx; x < endx; x++){
+          sum_red += line_buffer[x].red;
+          sum_green += line_buffer[x].green;
+          sum_blue += line_buffer[x].blue;
+        }
+      }
+      sum_red +=line_buffer[endx].red;
+      sum_green +=line_buffer[endx].green;
+      sum_blue +=line_buffer[endx].blue;
+    }else if (endx >= W){
+      endx = W-1;
+      sum_red -=line_buffer[startx-1].red;
+      sum_green -=line_buffer[startx-1].green;
+      sum_blue -=line_buffer[startx-1].blue;
+
+    }else{
+      sum_red += (line_buffer[endx].red-line_buffer[startx-1].red);
+      sum_green += (line_buffer[endx].green-line_buffer[startx-1].green);
+      sum_blue += (line_buffer[endx].blue-line_buffer[startx-1].blue);
+    }
+
+    // we save each pixel in the output image
+    float count_rec=1.0 / ((endx-startx+1)*(endy-starty+1));
+
+    imageOut->data[senterY*W + senterX].red = sum_red*count_rec;
+    imageOut->data[senterY*W + senterX].green = sum_green*count_rec;
+    imageOut->data[senterY*W + senterX].blue = sum_blue*count_rec;
+  }
+}
+
 
 // Perform the new idea:
 // The code in this function should run in parallel
@@ -57,112 +147,75 @@ void freeImage(AccurateImage *image){
 // Using OpenMP inside this function itself might be avoided
 // You may be able to do this only with a single OpenMP directive
 void performNewIdeaIteration(AccurateImage *imageOut, AccurateImage *imageIn,int size) {
-	int countIncluded = 0;
-	int offsetOfThePixel=0;
-	float sum_red = 0;
-	float sum_blue = 0;
-	float sum_green =0;
-	int W = imageIn->x;
+
+  int W = imageIn->x;
   int H = imageIn->y;
 
-  AccurateImage *image_buffer = (AccurateImage*)malloc(sizeof(AccurateImage));
-  image_buffer->data = (AccuratePixel*)malloc(W*H*sizeof(AccuratePixel));
-  memset(image_buffer->data, 0, W*H*sizeof(AccuratePixel));
+  // line buffer that will save the sum of some pixel in the column
+  AccuratePixel *line_buffer = (AccuratePixel*) malloc(W*sizeof(AccuratePixel));
+  memset(line_buffer,0,W*sizeof(AccuratePixel));
+  AccuratePixel *line_buffer1 = (AccuratePixel*) malloc(W*sizeof(AccuratePixel));
+  memset(line_buffer1,0,W*sizeof(AccuratePixel));
+  AccuratePixel *line_buffer2 = (AccuratePixel*) malloc(W*sizeof(AccuratePixel));
+  memset(line_buffer2,0,W*sizeof(AccuratePixel));
+  AccuratePixel *line_buffer3 = (AccuratePixel*) malloc(W*sizeof(AccuratePixel));
+  memset(line_buffer3,0,W*sizeof(AccuratePixel));
 
-  for(int y=0; y < size; y++){
-    for(int x=0; x<W; x++){
-      image_buffer->data[x].blue+=imageIn->data[y*W + x].blue;
-      image_buffer->data[x].red+=imageIn->data[y*W + x].red;
-      image_buffer->data[x].green+=imageIn->data[y*W + x].green;
+  int each = H/4;
+  // Iterate over each line of pixelx.
+  #pragma omp parallel for num_threads(4)
+  for(int senterY = 0; senterY < H; senterY++) {
+    // first and last line considered  by the computation of the pixel in the line senterY
+    if (omp_get_thread_num() == 0) {
+      if (senterY == 0) {
+        for(int y=0; y < size; y++) {
+          for(int i=0; i<W; i++){
+            line_buffer[i].blue+=imageIn->data[W*y+i].blue;
+            line_buffer[i].red+=imageIn->data[W*y+i].red;
+            line_buffer[i].green+=imageIn->data[W*y+i].green;
+          }
+        }
+      }
+      bar(senterY, size, line_buffer, imageIn, imageOut);
+    } else if (omp_get_thread_num() == 1) {
+      //printf("%d, ", senterY);
+      if (senterY == each) {
+        for(int y=senterY-size-1; y < senterY+size; y++) {
+          for(int i=0; i<W; i++){
+            line_buffer1[i].blue+=imageIn->data[W*y+i].blue;
+            line_buffer1[i].red+=imageIn->data[W*y+i].red;
+            line_buffer1[i].green+=imageIn->data[W*y+i].green;
+          }
+        }
+      }
+      bar(senterY, size, line_buffer1, imageIn, imageOut);
+    } else if (omp_get_thread_num() == 2) {
+      if (senterY == 2*each) {
+        for(int y=senterY-size-1; y < senterY+size; y++) {
+          for(int i=0; i<W; i++){
+            line_buffer2[i].blue+=imageIn->data[W*y+i].blue;
+            line_buffer2[i].red+=imageIn->data[W*y+i].red;
+            line_buffer2[i].green+=imageIn->data[W*y+i].green;
+          }
+        }
+      }
+      bar(senterY, size, line_buffer2, imageIn, imageOut);
+    } else {
+      if (senterY == 3*each) {
+        for(int y=senterY-size-1; y < senterY+size; y++) {
+          for(int i=0; i<W; i++){
+            line_buffer3[i].blue+=imageIn->data[W*y+i].blue;
+            line_buffer3[i].red+=imageIn->data[W*y+i].red;
+            line_buffer3[i].green+=imageIn->data[W*y+i].green;
+          }
+        }
+      }
+      bar(senterY, size, line_buffer3, imageIn, imageOut);
     }
   }
-  #pragma omp parallel for schedule(static, 1)
-	for(int senterY = 0; senterY < imageIn->y; senterY++) {
-		// first and last line considered  by the computation of the pixel in the line senterY
-		int starty = senterY-size;
-		int endy = senterY+size;
 
-		// Initialize and update the line_buffer.
-		// For OpenMP this might cause problems
-		// Separating out the initialization part might help
-		if (starty <=0){
-			starty = 0;
-			for(int i=0; i<imageIn->x; i++){
-				// add the next pixel of the next line in the column x
-				image_buffer->data[W*senterY + i].blue+=imageIn->data[W*endy+i].blue;
-				image_buffer->data[W*senterY + i].red+=imageIn->data[W*endy+i].red;
-				image_buffer->data[W*senterY + i].green+=imageIn->data[W*endy+i].green;
-			}
-
-		}
-
-		else if (endy >= imageIn->y ){
-			// for the last lines, we just need to subtract the first added line
-			endy = imageIn->y-1;
-			for(int i=0; i<imageIn->x; i++){
-				image_buffer->data[W*senterY + i].blue-=imageIn->data[W*(starty-1)+i].blue;
-				image_buffer->data[W*senterY + i].red-=imageIn->data[W*(starty-1)+i].red;
-				image_buffer->data[W*senterY + i].green-=imageIn->data[W*(starty-1)+i].green;
-			}
-		}else{
-			// general case
-			// add the next line and remove the first added line
-			for(int i=0; i<imageIn->x; i++){
-				image_buffer->data[W*senterY + i].blue+=imageIn->data[W*endy+i].blue-imageIn->data[W*(starty-1)+i].blue;
-				image_buffer->data[W*senterY + i].red+=imageIn->data[W*endy+i].red-imageIn->data[W*(starty-1)+i].red;
-				image_buffer->data[W*senterY + i].green+=imageIn->data[W*endy+i].green-imageIn->data[W*(starty-1)+i].green;
-			}
-		}
-		// End of line_buffer initialisation.
-
-
-		sum_green =0;
-		sum_red = 0;
-		sum_blue = 0;
-		for(int senterX = 0; senterX < imageIn->x; senterX++) {
-			// in this loop, we do exactly the same thing as before but only with the array line_buffer
-
-			int startx = senterX-size;
-			int endx = senterX+size;
-
-			if (startx <=0){
-				startx = 0;
-				if(senterX==0){
-					for (int x=startx; x < endx; x++){
-						sum_red += image_buffer->data[senterY*W + x].red;
-						sum_green += image_buffer->data[senterY*W + x].green;
-						sum_blue += image_buffer->data[senterY*W + x].blue;
-					}
-				}
-				sum_red +=image_buffer->data[senterY*W + endx].red;
-				sum_green +=image_buffer->data[senterY*W + endx].green;
-				sum_blue +=image_buffer->data[senterY*W + endx].blue;
-			}else if (endx >= imageIn->x){
-				endx = imageIn->x-1;
-				sum_red -=image_buffer->data[senterY*W + startx-1].red;
-				sum_green -=image_buffer->data[senterY*W + startx-1].green;
-				sum_blue -=image_buffer->data[senterY*W + startx-1].blue;
-
-			}else{
-				sum_red += (image_buffer->data[senterY*W + endx].red-image_buffer->data[senterY*W + startx-1].red);
-				sum_green += (image_buffer->data[senterY*W + endx].green-image_buffer->data[senterY*W + startx-1].green);
-				sum_blue += (image_buffer->data[senterY*W + endx].blue-image_buffer->data[senterY*W + startx-1].blue);
-			}
-
-			// we save each pixel in the output image
-			offsetOfThePixel = (W * senterY + senterX);
-			countIncluded=(endx-startx+1)*(endy-starty+1);
-
-			imageOut->data[offsetOfThePixel].red = sum_red/countIncluded;
-			imageOut->data[offsetOfThePixel].green = sum_green/countIncluded;
-			imageOut->data[offsetOfThePixel].blue = sum_blue/countIncluded;
-		}
-
-	}
-
-	// free memory
-  free(image_buffer);
-  free(image_buffer->data);
+  // free memory
+  free(line_buffer);
 }
 
 int getValue(float value)
