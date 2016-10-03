8e3a0436c154652765387ac2e999c8757f10db4c
Fredrik Pe Ingebrigtsen <fredripi@ntnu.no>
2016-10-03 13:16:40 +0200

    adf

--- it3105/project2/exercise3.py
+++ it3105/project2/exercise3.py
@@ -1,4 +1,6 @@
 import random
+import numpy
+import collections
 
 import gym
 
@@ -8,16 +10,25 @@ class QLearning:
         self.Q = Q
         self.action_space = action_space
 
-        self.e = 0.1
         self.discount = 0.99
-        self.learning_rate = 0.3
+        self.learning_rate = 0.8
+        self.epsilon_list = self.epsilon()
+
+    @staticmethod
+    def epsilon():
+        return numpy.linspace(0.1, 0, number_of_episodes)
 
     def value_iteration_update(self, state, action, reward, new_state):
         self.Q[state][action] += \
             self.learning_rate * (reward + self.discount * max(self.Q[new_state]) - self.Q[state][action])
 
-    def Q_e_greedy(self, state):
-        if random.random() > self.e:
+    def value_iteration_update_4(self, state, action, reward, new_state, episode_number):
+        self.Q[state][action] += \
+            self.learning_rate * (reward + self.discount * self.Q[new_state][self.Q_e_greedy(state, episode_number)] - self.Q[state][action])
+
+    def Q_e_greedy(self, state, episode_number):
+        eps = self.epsilon_list[episode_number]
+        if random.random() > eps:
             max_value = -1000
             max_indexes = []
             for i, e in enumerate(self.Q[state]):
@@ -38,33 +49,43 @@ myQ = []
 for s in range(16):
     myQ.append([0 for i in range(4)])
 
+number_of_episodes = 3000
 q_learning = QLearning(myQ, [0, 1, 2, 3])
 
 env = gym.make('FrozenLake-v0')
 
-for i_episode in range(1000):
+hundred_slice = collections.deque()
+hundred_counter = 1
+
+for i_episode in range(number_of_episodes):
     observation = env.reset()
 
+
+    episode_reward = 0
     for t in range(10000):
-        episode_reward = 0
         #env.render()
         #print(observation)
-        action = q_learning.Q_e_greedy(observation)
+        action = q_learning.Q_e_greedy(observation, i_episode)
         old_observation = observation
         observation, reward, done, info = env.step(action)
 
-        q_learning.value_iteration_update(old_observation, action, reward, observation)
-        if reward > 0:
-            print("reward", reward)
-        episode_reward += reward
+        q_learning.value_iteration_update_4(old_observation, action, reward, observation, i_episode)
 
         if done:
-            print("Episode finished after {} timesteps".format(t+1))
+            episode_reward += reward
+            #print("Episode finished after {} timesteps".format(t+1))
             break
 
-    print("Episode:", i_episode, "Total reward:", episode_reward)
+    hundred_slice.append(episode_reward)
+    if hundred_counter == 100:
+        print(sum(hundred_slice))
+        hundred_slice.popleft()
+    else:
+        hundred_counter += 1
 
-# 0 Left
-# 1 Down
-# 2 Right
-# 3 Up
+    #print("Episode:", i_episode, "Total reward:", episode_reward)
+
+    # 0 Left
+    # 1 Down
+    # 2 Right
+    # 3 Up
